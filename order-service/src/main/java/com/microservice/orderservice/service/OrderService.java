172f13c93a7142f48021ae73ed3980c3fba9b1b2
gustavoaquino <guuaquino@hotmail.com>
2023-04-13 17:03:25 -0300

    Updating OrderService | Creating bean webclient, and adding InventoryResponse to know if it will be possible to create an order.

--- order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.microservice.orderservice.service;
 
+import com.microservice.orderservice.dto.InventoryResponse;
 import com.microservice.orderservice.dto.OrderLineItemsDto;
 import com.microservice.orderservice.dto.OrderRequest;
 import com.microservice.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.microservice.orderservice.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,8 +21,9 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
-    public void placeOrder(OrderRequest orderRequest){
+    public void placeOrder(OrderRequest orderRequest) throws IllegalAccessException {
         final Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -29,7 +33,24 @@ public class OrderService {
                 .toList();
 
         order.setOrderLineItemsList(orderLineItems);
-        orderRepository.save(order);
+
+        final List<String> skuCodeList = orderLineItems.stream().map(OrderLineItems::getSkuCode).toList();
+
+        //call inventory service before save the order, because we need to validate that have something in stock
+        final InventoryResponse[] inventoryResponseArray = webClient.get()
+                .uri("http://localhost:8083/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCodeList", skuCodeList).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();// Make the communication synchronous
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+
+        if(Boolean.TRUE.equals(allProductsInStock)){
+            orderRepository.save(order);
+        }else{
+            throw new IllegalAccessException("Product is not in stock, please try again late");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {

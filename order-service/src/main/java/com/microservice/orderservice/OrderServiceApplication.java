69ccaf4850fdff1c2bf2548d39172d1034b41bdf
gustavoaquino <guuaquino@hotmail.com>
2023-04-15 19:29:41 -0300

    Enabling eureka server in our services.

--- order-service/src/main/java/com/microservice/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/microservice/orderservice/OrderServiceApplication.java
@@ -3,7 +3,9 @@ package com.microservice.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @EnableAutoConfiguration
 @SpringBootApplication
 public class OrderServiceApplication {
--- product-service/src/test/java/com/microservice/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/microservice/productservice/ProductServiceApplicationTests.java
@@ -6,7 +6,6 @@ import com.microservice.productservice.repository.ProductRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;

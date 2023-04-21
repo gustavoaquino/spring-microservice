c2754d15f66f2e02a6b42fa20005953e2a5bc33f
gustavoaquino <guuaquino@hotmail.com>
2023-04-21 15:31:21 -0300

    Updating order-service web client.

--- order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) throws IllegalAccessException {
         final Order order = new Order();
@@ -37,8 +37,8 @@ public class OrderService {
         final List<String> skuCodeList = orderLineItems.stream().map(OrderLineItems::getSkuCode).toList();
 
         //call inventory service before save the order, because we need to validate that have something in stock
-        final InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8083/api/inventory",
+        final InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCodeList", skuCodeList).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
